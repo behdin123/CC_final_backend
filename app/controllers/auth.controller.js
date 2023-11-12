53bef95b1c5ed1143730268803e3cf444f1eddbd
behdin123 <behdin2010@gmail.com>
2023-11-12 16:54:38 +0100

    commit to try

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -79,7 +79,7 @@ class AuthController{
             // Set the JWT token as a cookie
             res.cookie('jwt', token, {
                 httpOnly: true,
-                sameSite: 'none',
+                sameSite: 'lax',
                 secure: true,
                 maxAge: 3600000, // 1 hour
             });
